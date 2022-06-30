4e038d46831ba2341246e7cf8ce3ce6dcf2b9705
Azubuine Samuel <tochukwu.azubuine.237253@unn.edu.ng>
2022-06-30 09:30:16 +0100

    updated files

---
+++ Backend/week_4/OOP_csv.py
@@ -0,0 +1,115 @@
+import csv
+
+
+class CsvProfileClass:
+    headers = ['username', 'first_name', 'last_name', 'password', 'phone_number', 'date_of_birth']
+    def __init__(self) -> None:
+
+
+    # # 1. After successful signup, it should prompt the user to signin.
+    # 2  # . After successful signin, user should be presented with the options: Edit profile, Change password, Logout.
+    # 3.  # Edit profile should ask for more information like phone_number (required), address (optional), date of birth (
+    # # optional) and gender (compulsory)
+
+
+
+    def update_profile_prompt(self,user_credential):
+        update_choice = input("Please update your Profile:"
+                              "Choose action to perform"
+                              "1 - Edit Profile"
+                              "2- Change Password"
+                              "3- Logout")
+        if update_choice == 1:
+            self.edit_profile(user_credential)
+        elif update_choice == 2:
+            self.change_password(user_credential)
+        elif update_choice == 3:
+            self.logout(user_credential)
+
+    def signin(self,username,password):
+        data = self.get_data()
+        for profile in data:
+            if profile['username'] == username and profile['password'] == password:
+                print(f'Login successful')
+                username = user_credential
+                return username
+            else:
+                print(f"Invalid login credentials")
+
+    user_credential = self.signin()
+    update_profile_prompt()
+
+    def validate_confirm_password(self, password, confirm_password):
+        return password == confirm_password
+
+    def get_data(self):
+        with open('user_db.csv', 'r') as f:
+            csv_file = csv.DictReader(f)
+            return list(csv_file)
+
+    def save_data(self, **kwargs):
+        with open('user_db.csv', 'a', newline='\n') as f:
+            csv_file = csv.DictWriter(f, fieldnames=headers)
+            csv_file.writerow(kwargs)
+
+    def change_password(self,username):
+        new_password = self.get_password()
+        user_data = self.get_data()
+        for row in user_data:
+            if row["username"] == username:
+                # line_number = __newline
+                with open('csv.csv', 'a') as file:
+                    file_handler = csv.DictWriter(file)
+                    file_handler.writerow([])
+                    user_info_ = row
+                    # [row for row in user_data if ]
+                # old_password = input("please input old password")
+                # new_password = input("Please Input new password")
+                save_data()
+
+    def edit_profile(self,**kwargs):
+        phone_number = get_phone_number()
+        address = get_address()
+        date_of_birth = "".join(get_date_of_birth().split("-"))
+        gender = get_gender()
+        headers2 = ['phone_number', 'address', 'date_of_birth', 'gender']
+        # date_of_birth = str("".join(date_of_birth))
+        self.update_data(kwargs)
+
+    def signup(self,username,firstname,last_name,password, confirm_password):
+
+        # Validation starts here
+        if not self.validate_username(username):
+            print(f"Error! Username: {username} already exists.")
+            return False
+
+        if not self.validate_confirm_password(password, confirm_password):
+            print(f"Error! Password and confirm password are different")
+            return False
+
+        self.save_data(username=username, first_name=first_name, last_name=last_name, password=password)
+        self.signin()
+
+    def get_username():
+        return input("Enter your username: ")
+
+    def validate_username(self,username):
+        data = self.get_data()
+        for profile in data:
+            if profile['username'] == username:
+                return False
+        return True
+
+    # def get_first_name():
+    #     return input("Enter first name: ")
+    #
+    # def get_last_name():
+    #     return input("Enter last name: ")
+    #
+    # def get_password():
+    #     return input("Enter password: ")
+    #
+    # def get_confirm_password():
+    #     return input("Confirm password: ")
+
+
--- Backend/week_4/classes_assignment.py
+++ Backend/week_4/classes_assignment.py
@@ -1,17 +1,62 @@
-class UserProfile:
-    def __init__(self,firstname,lastname):
-        self.firstname = firstname
-        self.lastname = lastname
+profile_data ={"basheer@gmail.com":{"first_name" :"Basheer", "last_name":"Balogun","attendance": 11,"height": 46, "weight": 23,"age":22, "birthday": {"day":8, "month": "april",}},
+                "abdullahi@gmail.com":{"first_name" :"Abdullahi", "last_name":"Salam","attendance": 11,"height": 25, "weight": 23,"age":23, "birthday": {"day":8, "month": "May",}},
+                "faith@gmail.com":{"first_name" :"Faith", "last_name":"Adeosun","attendance": 11,"height": 50, "weight": 23,"age":23, "birthday": {"day":8, "month": "Agu",}},
+                "ahmad@gmail.com": {"first_name" :"Ahmad", "last_name":"Sharafudeen","attendance": 11,"height": 71, "weight": 23,"age":23, "birthday": {"day":8, "month": "July",}},
+                "toluwanimi@gmail.com":  {"first_name" :"Toluwanimi", "last_name":"Ogunbiyi","attendance": 11,"height": 34, "weight": 24,"age":21, "birthday": {"day":8, "month": "Nov",}},
+                "awwal@gmail.com": {"first_name" :"Awwal", "last_name":"Adeleke","attendance": 11,"height":49 ,"weight": 23,"age":23, "birthday": {"day":8, "month": "Dec",}},
+                "abdulwali@gmail.com": {"first_name" :"Abdulwali", "last_name":"Tajudeen","attendance": 11,"height": 78, "weight": 23,"age":27, 
+                "birthday":
+                 {
+                    "day":8, 
+                    "month": "Mar",
+                    }
+                    },
+                "abraham@gmail.com": {"first_name" :"Abraham", "last_name":"Adekunle","attendance": 11,"height": 65, "weight": 23,"age":23, "birthday": {"day":8, "month": "May",}},
+                "yusuff@gmail.com": {"first_name" :"Yusuff", "last_name":"Oyedele","attendance": 11,"height": 52, "weight": 23,"age":23, "birthday": {"day":8, "month": "Oct",}},
+                "adebusola@gmail.com": {"first_name" :"Adebusola", "last_name":"Adeyeye","attendance": 11,"height": 43, "weight": 23,"age":26, "birthday": {"day":8, "month": "Feb",}},
+                "lukman@gmail.com": {"first_name" :"Lukman", "last_name":"Abisoye","attendance": 11,"height": 35, "weight": 54,"age":29, "birthday": {"day":4, "month": "Jan",}}}
 
-    def update_firstname_and_lastname(self,lastname_update,firstname_update):
-            self.lastname = lastname_update
-            self.firstname = firstname_update
+class UserProfile:
+    
+    def __init__(self,data_dict):
+        self.data_dict = profile_data
+        
+
+    def update_firstname_and_lastname(self,lastname_update,firstname_update,user_email):
+        for email in self.data_dict:
+            if user_email == email:
+                self.data_dict[user_email]["first_name"] = firstname_update
+                self.data_dict[user_email]["lastname"] = lastname_update
             return 
 
     def list_of_fullnames(self):
-        self.firstname.upper()
-        print(f"{self.firstname.upper()}  {}")
+        for email in self.data_dict:
+            i =1
+            for (k,v) in enumerate(email):
+                print(f"User name {i} is {k} {v}")
+                i+=1
         return 
 
+
+    def add_profile(self,user_email,user_firstname,user_lastname,):
+        pass
+
+    def increase_attendance(self,user_email):
+        for email in self.data_dict:
+            if user_email == email:
+                self.data_dict[user_email]["attendance"] +=1
+            return 
+
+    def sort_by_month(self):
+        sorted_month_data = {}
+        for email in self.data_dict:
+            for birthday in email:
+                for (k,v) in enumerate(birthday):
+                    if k in sorted_month_data.keys():
+
+                        sorted_month_data[k].update(self.data_dict[email])
+                        list(sorted_month_data.values()).append(k)
+                    else:
+                        sorted_month_data[k] = {self.data_dict[email]}
 
     
